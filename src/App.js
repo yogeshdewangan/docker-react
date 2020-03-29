cce15a31f3f3ca9cbd463b56f2048f25d92ad172
yogesh.dewangan <yogeshdewangan97@gmail.com>
2020-03-29 19:25:07 +0530

    Fixing by adding 'learn react' text in test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 		<h1 className="App-title"> Welcome to React </h1>
 		</header>
         <p className="App-intro">
-          I was changed on the Feature branch
+          I was changed on the Feature branch learn react
         </p>
        
     </div>
